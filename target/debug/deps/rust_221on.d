256e9c5a98a35c6abe62294e38b9d4b7ac254aad
SL-Angelina12 <angelinaslushniak08@gmail.com>
2024-10-23 22:03:53 +0300

    -

---
+++ target/debug/deps/rust_221on.d
@@ -0,0 +1,5 @@
+C:\Users\Master\RustroverProjects\rust-221 on\target\debug\deps\rust_221on.exe: src/main.rs
+
+C:\Users\Master\RustroverProjects\rust-221 on\target\debug\deps\rust_221on.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rust-221on.d
@@ -0,0 +1,1 @@
+C:\Users\Master\RustroverProjects\rust-221\ on\target\debug\rust-221on.exe: C:\Users\Master\RustroverProjects\rust-221\ on\src\main.rs
